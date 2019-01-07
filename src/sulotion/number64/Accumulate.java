1c53c43b3c4173596a97bc33c0b5a1dace48b39c
naget <cloved9999@gmail.com>
2019-01-07 19:24:05 +0800

    面试题64：求1+2+。。。+n（&&短路求值）

--- src/sulotion/number64/Accumulate.java
+++ src/sulotion/number64/Accumulate.java
@@ -8,20 +8,18 @@ package sulotion.number64;
  * 题目：求1+2+…+n，要求不能使用乘除法、for、while、if、else、switch、case
  * 等关键字及条件判断语句（A?B:C）。
  *
- * 利用构造函数求解，实现递归
+ * 短路求值
  */
 public class Accumulate {
     private static int n=0;
     private static int Sum=0;
-    public Accumulate(int N){
-        if (N>n){
-            n++;
-            Sum += n;
-           new Accumulate(N);
-        }else System.out.println("1到n相加等于"+Sum);
+    public static int getSum(int n){
+        int result = 0;
+        result = n;
+        boolean value = (n!=0)&&1==(result += getSum(n-1));
+        return result;
     }
-
     public static void main(String[] args) {
-        new Accumulate(100);
+        System.out.println(getSum(100));
     }
 }
