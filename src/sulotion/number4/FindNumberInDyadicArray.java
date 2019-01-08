a8d3f05623f32de857a0364c15dc550261f15e27
naget <cloved9999@gmail.com>
2019-01-08 14:17:24 +0800

    添加文字说明

--- src/sulotion/number4/FindNumberInDyadicArray.java
+++ src/sulotion/number4/FindNumberInDyadicArray.java
@@ -2,7 +2,10 @@ package sulotion.number4;
 
 /**
  * created by tianfeng on 2018/11/20
- * 在一个特殊的二维数组里查找一个数
+ * 面试题4：二维数组中的查找
+ * 题目：在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按
+ * 照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个
+ * 整数，判断数组中是否含有该整数。
  */
 public class FindNumberInDyadicArray {
     public static void find(int[][] array,int number){
--- src/sulotion/number7/RebuildBT.java
+++ src/sulotion/number7/RebuildBT.java
@@ -4,6 +4,11 @@ import sulotion.number32.Number32;
 
 /**
  * created by tianfeng on 2018/12/3
+ * 面试题7：重建二叉树
+ * 题目：输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输
+ * 入的前序遍历和中序遍历的结果中都不含重复的数字。例如输入前序遍历序列{1,
+ * 2, 4, 7, 3, 5, 6, 8}和中序遍历序列{4, 7, 2, 1, 5, 3, 8, 6}，则重建出
+ * 图2.6所示的二叉树并输出它的头结点。
  */
 public class RebuildBT {
     static class TreeNode{
